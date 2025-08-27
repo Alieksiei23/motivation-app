e8669e74eae0e35022a2331dd8ee20abf07e595b
Aliaksei Sidor <lesha.sidor57@gmail.com>
2025-08-27 21:07:05 +0300

    with backgroundtasks

--- sending/send.py
+++ sending/send.py
@@ -2,7 +2,7 @@ import smtplib
 import random
 from email.mime.text import MIMEText
 from sqlalchemy import text
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 
 from settings import setting, engine, AsyncSession
 
@@ -12,19 +12,22 @@ sender = setting.LOGIN
 password = setting.EMAIL_PASSWORD
 
 
+def sending_letter(user_emails: list, letters: list) -> None:
+    server = smtplib.SMTP('smtp.mail.ru', 587)
+    server.starttls()
+    server.login(sender, password)
+    for email in user_emails:
+        letter = MIMEText(random.choice(letters)).as_string()
+        server.sendmail(sender, email, letter)
+
+
 @router.post('/letter/')
-async def send_all_mail():
+async def sending_to_all_email(bg_task: BackgroundTasks):
     async with AsyncSession(engine) as conn:
-        server = smtplib.SMTP('smtp.mail.ru', 587)
-        server.starttls()
-        server.login(sender, password)
         users_email = await conn.execute(text("SELECT email FROM users"))
         letters = await conn.execute(text("SELECT letter FROM letters"))
         result = letters.scalars().all()
-        print(result)
-        for email in users_email.scalars().all():
-            letter = MIMEText(random.choice(result)).as_string()
-            server.sendmail(sender, email, letter)
+        bg_task.add_task(sending_letter, users_email, result)
         return {
             'send': 'success'
         }
